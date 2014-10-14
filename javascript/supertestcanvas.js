f951024259a63b82b588af30550edf2d6dc55744
root <root@comp.lancs.ac.uk>
2014-10-14 12:10:08 +0100

    parse

--- javascript/supertestcanvas.js
+++ javascript/supertestcanvas.js
@@ -57,9 +57,16 @@ socket.on('switchResponse', function(data) {
 		switchBackground(data.bgimage);
 		if(data.reason=="next" && (screenNumber+1 <= data.max)) screenNumber++;
 		else if(data.reason=="back") screenNumber--;
-		collaborative = data.collaborative;
+		collaborative = JSON.parse(data.collaborative);
 		
 		alert("This is screen " + screenNumber + " and it's setting for collaborative is " + collaborative);
+		
+		var testCollaborative;
+		
+		if(collaborative) testCollaborative = true;
+		else testCollaborative = false;
+		
+		alert("And collaborative evaluated to " + testCollaborative); 
 	}
 });
 
