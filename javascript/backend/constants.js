8cfef57e02c3597f4574d4a228604b0fd3a1e11f
Habib Naderi <habib@cs.auckland.ac.nz>
2014-12-09 09:13:36 +1300

    Module for constants.

---
+++ javascript/backend/constants.js
@@ -0,0 +1,19 @@
+/**
+ * New node file
+ */
+
+function define(name, value) {
+    Object.defineProperty(exports, name, {
+        value:      value,
+        enumerable: true
+    });
+}
+
+define("PRAC_AREA", 0);
+define("PIC_CON", 1);
+define("PIC_COMP", 2);
+define("PAR_LINES", 3);
+define("IDEA_GEN", 4);
+define("DES_CHAL", 5);
+define("ALT_USES", 6);
+ 
