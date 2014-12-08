6e822b738c3a1c0bcdd78d5fecd30feb1a5834e8
Habib Naderi <habib@cs.auckland.ac.nz>
2014-12-08 15:43:18 +1300

    module for common functions.

---
+++ javascript/backend/commons.js
@@ -0,0 +1,29 @@
+/**
+ * New node file
+ */
+
+module.exports = function(context)
+{
+	return {
+		testComplete: function() {
+	    	context.channel.sendToTeam(context.session.TeamID, 'TestCompleteMsg');
+	    	context.channel.sendToMinID(context.session.TeamID, 'GetResultsReq');
+		},
+		
+		setTestTime: function() {
+	        context.rdb.setTime(context.session.TeamID, 10000);
+		},
+		
+		sendBackendReady: function() {
+	        context.channel.sendToUser(context.session.AccessCode, "BackendReadyMsg");
+		},		
+		
+		setupTestTimer: function(callback, args) {
+	        var timer = setInterval(function() {
+	        	context.rdb.updateTime(context.session.TeamID, callback, args);
+	        }, 2*1000);			
+	        return timer;
+		}
+		
+	};	
+};
